588d64516223316c60cd3ab394e266d060153c46
CloudBeeOps <cloudbeeops@gmail.com>
2022-07-26 22:38:57 +0530

    update_on_front_page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> CloudBeeOps is in construction. For more, kindly reach out to 'admin@cloudbeeops.com'</p>
         <a
           className="App-link"
           href="https://reactjs.org"
