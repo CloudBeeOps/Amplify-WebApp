9153c66b83ca6eeee342765b6de37b5b04fb8f89
CloudBeeOps <cloudbeeops@gmail.com>
2022-07-27 00:15:05 +0530

    front_end_updat

--- src/App.js
+++ src/App.js
@@ -8,18 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p> CloudBeeOps is in construction. For more, kindly reach out to 'admin@cloudbeeops.com'</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> CloudBeeOps is in construction. For more, kindly reach out 'admin@cloudbeeops.com'</p>
       </header>
     </div>
   );
